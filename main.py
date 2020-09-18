315dac9ce3a68357395b933261b09dd8a1794949
ctc5367 <ctc5367@psu.edu>
2020-09-18 18:35:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def digit_sum(n):
+  if n == 0:
+    return  0
+  else:
+    return (n%10 + digit_sum(int(n/10)))
+
+def run():
+  num = input("Enter an int: ")
+  num = int(num)
+  print(f"sum of the digits of {num} is {digit_sum(num)}.")
+
+if __name__ ==  "__main__":
+  run()
