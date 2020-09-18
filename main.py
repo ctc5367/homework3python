3e01c457373f248cafe87065b9b5d0677e170a42
ctc5367 <ctc5367@psu.edu>
2020-09-18 18:42:15 +0000

    int division

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def digit_sum(n):
   if n == 0:
     return  0
   else:
-    return (n%10 + digit_sum(int(n/10)))
+    return (n%10 + digit_sum(int(n//10)))
 
 def run():
   num = input("Enter an int: ")
